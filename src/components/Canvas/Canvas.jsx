9bdf112002d4e0be95ae8605ccb85f120ba67a1b
Dmytro <ihnatenko.dev@gmail.com>
2023-02-20 16:23:22 +0200

    Create canvasRef, save canvas to store

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect, useRef } from 'react';
+import canvasState from '../../store/canvasState';
 import s from './Canvas.module.scss';
 
-const Canvas = () => {
+const Canvas = observer(() => {
+	const canvasRef = useRef(null);
+
+	useEffect(() => {
+		canvasState.setCanvas(canvasRef.current);
+	}, []);
+
 	return (
 		<section className={s.wrapper}>
-			<canvas width={600} height={400}></canvas>
+			<canvas ref={canvasRef} width={600} height={400}></canvas>
 		</section>
 	);
-};
+});
 
 export default Canvas;
