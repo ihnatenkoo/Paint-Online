e5cff75cd244d89114f00fc46654e2d8843c826d
Dmytro <ihnatenko.dev@gmail.com>
2023-02-20 16:08:06 +0200

    ToolBar - add icons

--- src/components/ToolBar/ToolBar.jsx
+++ src/components/ToolBar/ToolBar.jsx
@@ -2,7 +2,48 @@ import React from 'react';
 import s from './ToolBar.module.scss';
 
 const ToolBar = () => {
-	return <section className={s.tool}>ToolBar</section>;
+	return (
+		<section className={s.tool}>
+			<nav className={s.nav}>
+				<div className={s.nav__section}>
+					<button>
+						<span className='material-icons-outlined'>edit</span>
+					</button>
+					<button>
+						<span className='material-icons-outlined'>
+							check_box_outline_blank
+						</span>
+					</button>
+					<button>
+						<span className='material-icons-outlined'>circle</span>
+					</button>
+					<button>
+						<span className='material-icons-outlined'>timeline</span>
+					</button>
+					<button>
+						<span className='material-icons-outlined'>auto_fix_normal</span>
+					</button>
+					<button>
+						<label>
+							<span className='material-icons-outlined'>color_lens</span>
+							<input type='color' className={s.color_input} />
+						</label>
+					</button>
+				</div>
+				<div className={s.nav__section}>
+					<button>
+						<span className='material-icons-outlined'>undo</span>
+					</button>
+					<button>
+						<span className='material-icons-outlined'>redo</span>
+					</button>
+					<button>
+						<span className='material-icons-outlined'>save</span>
+					</button>
+				</div>
+			</nav>
+		</section>
+	);
 };
 
 export default ToolBar;
