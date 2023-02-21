ad80b4349b1d08800e5e9e255ff21d73389da578
Dmytro <ihnatenko.dev@gmail.com>
2023-02-21 16:33:29 +0200

    Client: add router, create uniq session

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path='/:id' element={<App />}></Route>
+				<Route
+					path='*'
+					element={<Navigate to={`f${(+new Date()).toString(16)}`} replace />}
+				/>
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
