c10895820c9904f16d93800c59470b8b67928373
Dmytro <ihnatenko.dev@gmail.com>
2023-02-21 10:45:35 +0200

    Create line-width input in SettingBar

--- src/components/SettingBar/SettingBar.jsx
+++ src/components/SettingBar/SettingBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import toolState from '../../store/toolState';
 import s from './SettingBar.module.scss';
 
 const SettingBar = () => {
-	return <section className={s.bar}>SettingBar</section>;
+	return (
+		<section className={s.bar}>
+			<label htmlFor='line-width'>Line Width:</label>
+			<input
+				onChange={(e) => toolState.setLineWidth(e.target.value)}
+				style={{ marginLeft: '12px', width: '40px', textAlign: 'center' }}
+				id='line-width'
+				type='number'
+				min={1}
+				max={50}
+				defaultValue={1}
+			/>
+		</section>
+	);
 };
 
 export default SettingBar;
