02235c317a2428d64d5b4697cfeb06223972ab0f
Dmytro <ihnatenko.dev@gmail.com>
2023-02-22 19:31:24 +0200

    Highlight active tool in ToolBar

--- client/src/components/ToolBar/ToolBar.jsx
+++ client/src/components/ToolBar/ToolBar.jsx
@@ -1,56 +1,68 @@
-import React from 'react';
+import cn from 'classnames';
+import React, { useState } from 'react';
 import canvasState from '../../store/canvasState';
 import toolState from '../../store/toolState';
 import { Brush, Circle, Eraser, Line, Rectangle } from '../../tools/';
 import s from './ToolBar.module.scss';
 
 const ToolBar = () => {
+	const [activeTool, setActiveTool] = useState('brush');
+
+	const onClickHandler = (name, tool) => {
+		setActiveTool(name);
+		toolState.setTool(
+			new tool(canvasState.canvas, canvasState.socket, canvasState.sessionId)
+		);
+	};
+
 	return (
 		<section className={s.tool}>
 			<nav className={s.nav}>
 				<div className={s.nav__section}>
-					<button
-						onClick={() =>
-							toolState.setTool(
-								new Brush(
-									canvasState.canvas,
-									canvasState.socket,
-									canvasState.sessionId
-								)
-							)
-						}
-					>
-						<span className='material-icons-outlined'>edit</span>
+					<button onClick={() => onClickHandler('brush', Brush)}>
+						<span
+							className={cn('material-icons-outlined', {
+								[s.active]: activeTool === 'brush',
+							})}
+						>
+							edit
+						</span>
 					</button>
-					<button
-						onClick={() =>
-							toolState.setTool(
-								new Rectangle(
-									canvasState.canvas,
-									canvasState.socket,
-									canvasState.sessionId
-								)
-							)
-						}
-					>
-						<span className='material-icons-outlined'>
+					<button onClick={() => onClickHandler('rectangle', Rectangle)}>
+						<span
+							className={cn('material-icons-outlined', {
+								[s.active]: activeTool === 'rectangle',
+							})}
+						>
 							check_box_outline_blank
 						</span>
 					</button>
-					<button
-						onClick={() => toolState.setTool(new Circle(canvasState.canvas))}
-					>
-						<span className='material-icons-outlined'>circle</span>
+					<button onClick={() => onClickHandler('circle', Circle)}>
+						<span
+							className={cn('material-icons-outlined', {
+								[s.active]: activeTool === 'circle',
+							})}
+						>
+							circle
+						</span>
 					</button>
-					<button
-						onClick={() => toolState.setTool(new Line(canvasState.canvas))}
-					>
-						<span className='material-icons-outlined'>timeline</span>
+					<button onClick={() => onClickHandler('line', Line)}>
+						<span
+							className={cn('material-icons-outlined', {
+								[s.active]: activeTool === 'line',
+							})}
+						>
+							timeline
+						</span>
 					</button>
-					<button
-						onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}
-					>
-						<span className='material-icons-outlined'>auto_fix_normal</span>
+					<button onClick={() => onClickHandler('eraser', Eraser)}>
+						<span
+							className={cn('material-icons-outlined', {
+								[s.active]: activeTool === 'eraser',
+							})}
+						>
+							auto_fix_normal
+						</span>
 					</button>
 				</div>
 				<div className={s.nav__section}>
